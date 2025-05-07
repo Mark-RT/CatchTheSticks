a996194f8c5968f01d0c9925cb13bb210f110520
Mark Slobodyanik <markotav20@gmail.com>
2025-05-07 11:28:23 +0300

    update trash

--- src/main.cpp
+++ src/main.cpp
@@ -4,13 +4,19 @@
 
 void setup()
 {
+  Serial.begin(9600);
   pinMode(led, OUTPUT);
 }
 
 void loop()
 {
-  digitalWrite(led, 1);
-  delay(500);
-  digitalWrite(led, 0);
-  delay(500);
+  for (int i = 0; i < 15; i++)
+  {
+    digitalWrite(led, 1);
+    delay(500);
+    Serial.println("Counter: " + String(i));
+    //Serial.println(i);
+    digitalWrite(led, 0);
+    delay(400);
+  }
 }
